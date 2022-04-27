e3dd2fc3f697c38236496419b25149d40aa445e7
Gbeka <97500383+Gbeka@users.noreply.github.com>
2022-04-27 14:06:59 +0800

    solved_issues

--- js.js
+++ js.js
@@ -13,10 +13,10 @@ window.onload = () => {
 
 button_input.forEach((button_class) => {
     button_class.addEventListener("click", () => {
-        if (equal_pressed == 1) {
-            input.value = "";
-            equal_pressed = 0;
-        }
+        // if (equal_pressed == 1) {
+        //     input.value = "";
+        //     equal_pressed = 0;
+        // }
 
         input.value += button_class.value;
 
